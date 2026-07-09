10f19fc1ca2e3e503385948fdb0be1b98ffe5002
Vicente Barrientos <vtbarrientos@uc.cl>
2026-07-09 17:40:08 -0400

    Copy Prisma query engines to api/ bundle folder in build script

---
+++ apps/api/scripts/copy-engines.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const srcDir = path.resolve(__dirname, "../../database/src/generated/client");
+const destDir = path.resolve(__dirname, "../api");
+
+if (!fs.existsSync(srcDir)) {
+  console.log(`[copy-engines] Source directory ${srcDir} does not exist. Skipping.`);
+  process.exit(0);
+}
+
+if (!fs.existsSync(destDir)) {
+  fs.mkdirSync(destDir, { recursive: true });
+}
+
+fs.readdirSync(srcDir).forEach((file) => {
+  if (file.endsWith(".node") || file.includes("query_engine")) {
+    const srcFile = path.join(srcDir, file);
+    const destFile = path.join(destDir, file);
+    console.log(`[copy-engines] Copying ${file} to ${destDir}`);
+    fs.copyFileSync(srcFile, destFile);
+  }
+});
